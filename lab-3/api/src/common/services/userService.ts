5be3068b3926a507da9f11edb1a4f17f7d0b3648
Konstantin Shulga <mrkonstantinsh@gmail.com>
2021-04-14 16:41:54 +0300

    feat(lab-3): implement user service

---
+++ lab-3/api/src/common/services/userService.ts
@@ -0,0 +1,31 @@
+import * as bcrypt from 'bcryptjs';
+import { getConnection, getRepository } from 'typeorm';
+
+import { User } from '../models/entities/user';
+
+export default class UserService {
+  public async createNewUser(email: string, password: string): Promise<void> {
+    const salt = bcrypt.genSaltSync(10);
+    const passwordHash = bcrypt.hashSync(password, salt);
+
+    const user = new User();
+    user.email = email;
+    user.passwordHash = passwordHash;
+
+    await getConnection().manager.save(user);
+  }
+
+  public async getUserById(id: number): Promise<User> {
+    return await getRepository(User)
+      .createQueryBuilder('users')
+      .where('users.id = :id', { id: id })
+      .getOne();
+  }
+
+  public async getUserByEmail(email: string): Promise<User> {
+    return await getRepository(User)
+      .createQueryBuilder('users')
+      .where('users.email = :email', { email: email })
+      .getOne();
+  }
+}
